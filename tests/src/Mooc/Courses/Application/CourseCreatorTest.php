89735e7e2bdc3af2450e2bd8ed5f9f8c9c10275c
jorgechavezrnd <jorgechavezrnd@gmail.com>
2020-10-06 22:02:54 -0400

    Refactor for CourseCreatorTest

--- tests/src/Mooc/Courses/Application/CourseCreatorTest.php
+++ tests/src/Mooc/Courses/Application/CourseCreatorTest.php
@@ -5,24 +5,43 @@ declare(strict_types = 1);
 namespace CodelyTv\Tests\Mooc\Courses\Application;
 
 use CodelyTv\Mooc\Courses\Application\CourseCreator;
+use CodelyTv\Mooc\Courses\Domain\Course;
 use CodelyTv\Mooc\Courses\Domain\CourseRepository;
 use CodelyTv\Tests\Mooc\Courses\Domain\CourseMother;
+use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
 
 final class CourseCreatorTest extends TestCase
 {
+    private $repository;
+    private $creator;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->creator = new CourseCreator($this->repository());
+    }
+
     /** @test */
     public function it_should_create_a_valid_course(): void
     {
-        $repository = $this->createMock(CourseRepository::class);
-        $creator = new CourseCreator($repository);
-
         $request = CreateCourseRequestMother::random();
-
         $course = CourseMother::fromRequest($request);
 
-        $repository->method('save')->with($course);
+        $this->shouldSave($course);
 
-        $creator->__invoke($request);
+        $this->creator->__invoke($request);
+    }
+
+    private function shouldSave(Course $course): void
+    {
+        $this->repository()->method('save')->with($course);
+    }
+
+    /** @return CourseRepository|MockObject */
+    private function repository(): MockObject
+    {
+        return $this->repository = $this->repository ?: $this->createMock(CourseRepository::class);
     }
 }
